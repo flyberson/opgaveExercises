f823b5c2c1f93e19248a61ede66f990d2854b645
flyberson <flyberson@flyberpc>
2017-03-01 11:03:51 +0100

    Merge origin/master
    Conflicts:
    	src/miniprojekt_V2/Film.java
    	src/miniprojekt_V2/Tider.java

--- src/exerciseabstractfactory/Abstractfactorymain.java
+++ src/exerciseabstractfactory/Abstractfactorymain.java
@@ -2,32 +2,60 @@
  */
 package exerciseabstractfactory;
 
+import sun.security.acl.WorldGroupImpl;
+
 /**
  *
  * @author flyberson
  */
 public class Abstractfactorymain {
+    static InfantryFactory hf1;
+    static InfantryFactory of1;
+    static InfantryUnit Hunit1;
+    static InfantryUnit Hunit2;
+    static InfantryUnit Ounit1;
+    static InfantryUnit Ounit2;
+    static World world;
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Archer archer1 = new Archer();
-        Archer archer2= new Archer();
-        RockHauler rh1= new RockHauler();
-        RockHauler rh2= new RockHauler();
-        SpearThrower st1 = new SpearThrower();
-        SpearThrower st2 = new SpearThrower();
-        CrossbowMan cm1 = new CrossbowMan();
-        CrossbowMan cm2 = new CrossbowMan();
-        Musketeer m1 = new Musketeer();
-        Musketeer m2 = new Musketeer();
         
-        while (rh1.hits>0 && m2.hits >0){
-            m2.attack(rh1);
-            rh1.attack(m2);
-            System.out.println("Musk hits "+m2.hits);
-            System.out.println("Rock hits "+rh1.hits);
+        world = World.getInstance();
+//        world.setName("The World");
+    world.setName("Earth");
+        System.out.println(world.getName());
+        
+        FactoryFactory ff = new FactoryFactory();
+        hf1 = ff.createInfanftryFactory(1);
+        of1 = ff.createInfanftryFactory(2);
+        
+        Hunit1 = hf1.createUnit(0);
+        Hunit2 = hf1.createUnit(2);
+        Ounit1 = of1.createUnit(3);
+        Ounit2 = of1.createUnit(4);
+        
+        while((Hunit1.hits>0 || Hunit2.hits > 0) && (Ounit1.hits>0 || Ounit2.hits>0)){
+            if(Hunit1.hits>0){
+                Hunit1.attack(Ounit1);
+                Hunit1.attack(Ounit2);
+            }
+             if(Hunit2.hits>0){
+                Hunit2.attack(Ounit1);
+                Hunit2.attack(Ounit2);
+            }
+              if(Ounit1.hits>0){
+                Ounit1.attack(Hunit1);
+                Ounit1.attack(Hunit2);
+            }
+               if(Ounit2.hits>0){
+                Ounit2.attack(Hunit1);
+                Ounit2.attack(Hunit2);
+            }
+            System.out.println("Human1 ="+Hunit1.hits + "  Human 2 ="+ Hunit2.hits 
+                    + "  Orc1 =" + Ounit1.hits + "  Orc2 =" + Ounit2.hits);
         }
         
     }
